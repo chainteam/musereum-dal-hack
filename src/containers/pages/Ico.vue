956ddd3776da2cbdca4062102a00c9769c0800d5
Borzenko Anton <borzenko_a@bk.ru>
2017-12-16 17:54:50 +0300

    Added ICO card

---
+++ src/containers/pages/Ico.vue
@@ -0,0 +1,28 @@
+<div class="row ico">
+    <div class="col-md-8">
+        <div class="ico-logo">
+            <img src="123.png">
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="ico-info">
+            <div class="ico-info-progress">
+                <div class="ico-info-progress-success" width="90%"></div>
+            </div>
+            <div class="ico-info-collected_etm">54,183.92 ETM</div>
+            <div class="ico-info-collected_label">Collected (60,000 ETM Hard cap)</div>
+            <div class="ico-info-people_counter">463</div>
+            <div class="ico-info-people_label">Backers</div>
+            
+            <div class="ico-info-time">08:20:37:09</div>
+            <div class="ico-info-time_label">Until end</div>
+            
+            <button class="btn">Back this track</button>
+        </div>
+        <div class="ico-author">
+            <div class="ico-author-header">Author</div>
+            <img class="ico-author-img" src="123.jpg">
+            <div class="ico-author-name" >Camila Cabello</div>
+        </div>
+    </div>
+</div>
